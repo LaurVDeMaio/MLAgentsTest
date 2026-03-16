d3e118a859c357e6b27bf9a20d69527230b5a4f6
Lauren Victoria DeMaio <104632088+LaurVDeMaio@users.noreply.github.com>
2026-03-16 11:19:25 -0400

    adding onnx

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -8,8 +8,8 @@ public class Stats : MonoBehaviour
     List<int> Goals;
     int numEpisodes = 0;
 
-    int highest = 0;
-    int highepisode = 0;
+    // int highest = 0;
+    // int highepisode = 0;
     System.TimeSpan hightime;
 
     System.Diagnostics.Stopwatch stopwatch;
@@ -17,7 +17,7 @@ public class Stats : MonoBehaviour
     string csv = "";
     string csv_filename = "";
 
-    //int ohno, goal = 0;
+    int ohno, goal = 0;
 
     float rewardTotal = 0;
     int totalRewads = 0;
@@ -64,52 +64,58 @@ public class Stats : MonoBehaviour
     public void AddGoal(int agentEpisode, int x, float reward)
     {
         //testing code
-        // if (x == 1)
-        // {
-        //     goal++;
+        if (x == 1)
+        {
+            goal++;
             
-        // }
+        }
 
-        // else if (x == 0)
-        // {
-        //     ohno++;
+        else if (x == 0)
+        {
+            ohno++;
            
-        // }
+        }
 
-        // Debug.Log("Goals: " + goal + " Fails: " + ohno + " Total: " + (goal+ohno));
+        Debug.Log("Goals: " + goal + " Fails: " + ohno + " Total: " + (goal+ohno));
 
         rewardTotal += reward;
         totalRewads++;
 
-        float rewardAverage = (rewardTotal / totalRewads);
+        if(goal+ohno == 100) {
+            Debug.Log("<color=#00ff00>End!</color>");
+            // end play session
+            UnityEditor.EditorApplication.ExitPlaymode();
+        }
+
+        //float rewardAverage = (rewardTotal / totalRewads);
 
         //training code
-        Goals.RemoveAt(0);
+        // Goals.RemoveAt(0);
 
-        if(x == 1) {
-            Goals.Add(1);
-        }
-        else if(x == 0) {
-            Goals.Add(0);
-        }
+        // if(x == 1) {
+        //     Goals.Add(1);
+        // }
+        // else if(x == 0) {
+        //     Goals.Add(0);
+        // }
 
-        int total = 0;
-        foreach (var g in Goals)
-        {
-            total += g;
-        }
+        // int total = 0;
+        // foreach (var g in Goals)
+        // {
+        //     total += g;
+        // }
 
-        if (total > highest)
-        {
-            highest = total;
-            hightime = stopwatch.Elapsed;
-            highepisode = agentEpisode;
-            csv += highest + "," + highepisode + "," + ConvertTime(hightime) + "\n";
-            System.IO.File.WriteAllText(csv_filename, csv);
-        }
+        // if (total > highest)
+        // {
+        //     highest = total;
+        //     hightime = stopwatch.Elapsed;
+        //     highepisode = agentEpisode;
+        //     csv += highest + "," + highepisode + "," + ConvertTime(hightime) + "\n";
+        //     System.IO.File.WriteAllText(csv_filename, csv);
+        // }
 
-        Debug.Log("Goals: " + total + "/100 -- " + highest + " -- " + ConvertTime(hightime) + " -- " + highepisode);
-        Debug.Log("Reward Average: " + rewardAverage.ToString("F2"));
+        //Debug.Log("Goals: " + total + "/100 -- " + highest + " -- " + ConvertTime(hightime) + " -- " + highepisode);
+        // Debug.Log("Reward Average: " + rewardAverage.ToString("F2"));
         /*if (total == 100)
         {
             Debug.Log("<color=#00ff00>Perfect Score!</color>");
